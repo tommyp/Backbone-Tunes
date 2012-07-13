aac099aed07131f8f536e9321d35503b03e1049d
Tommy Palmer <hi@tommyp.org>
2012-07-13 15:11:34 +0100

    adding a playlist view

--- public/js/Tunes.js
+++ public/js/Tunes.js
@@ -152,6 +152,28 @@
     }
   });
 
+  window.PlaylistAlbumView = AlbumView.extend({});
+
+  window.PlaylistView = Backbone.View.extend({
+    tagName: 'section',
+    className: 'playlist',
+
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.template = _.template($('#playlist-template').html());
+      this.collection.bind('reset', this.render);
+      this.player = this.options.player;
+      this.library = this.options.library;
+    },
+
+    render: function() {
+      $(this.el).html(this.template(this.player.toJSON()));
+      this.$('button.play').toggle(this.player.isStopped());
+      this.$('button.pause').toggle(this.player.isPlaying());
+      return this;
+    }
+  });
+
   window.LibraryView = Backbone.View.extend({
     tagName: 'section',
     className: 'library',
@@ -185,6 +207,12 @@
     },
 
     initialize: function() {
+        this.playlistView = new PlaylistView({
+          collection: window.player.playlist,
+          player: window.player,
+          library: window.library
+        });
+
         this.libraryView = new LibraryView({
           collection: window.library
         });
@@ -193,6 +221,7 @@
     home: function() {
       var $container = $('#container');
       $container.empty();
+      $container.append(this.playlistView.render().el);
       $container.append(this.libraryView.render().el);
     },
 
